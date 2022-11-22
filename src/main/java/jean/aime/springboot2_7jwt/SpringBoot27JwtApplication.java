14123de51bf22825c7c53ba5f506476fa79475d5
Jean Aimé RAVOMANANA <jeanaime.ravomanana@gmail.com>
2022-11-22 11:19:57 +0300

    initial commit

---
+++ src/main/java/jean/aime/springboot2_7jwt/SpringBoot27JwtApplication.java
@@ -0,0 +1,13 @@
+package jean.aime.springboot2_7jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot27JwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot27JwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jean/aime/springboot2_7jwt/SpringBoot27JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package jean.aime.springboot2_7jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot27JwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
