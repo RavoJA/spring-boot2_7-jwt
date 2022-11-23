de0a1788fee7c0cc4500dbeb2cca05b6e094e5e0
Jean Aimé RAVOMANANA <jeanaime.ravomanana@gmail.com>
2022-11-23 15:18:56 +0300

    in memory user

--- src/main/java/jean/aime/springboot2_7jwt/SpringBoot27JwtApplication.java
+++ src/main/java/jean/aime/springboot2_7jwt/SpringBoot27JwtApplication.java
@@ -1,9 +1,12 @@
 package jean.aime.springboot2_7jwt;
 
+import jean.aime.springboot2_7jwt.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class SpringBoot27JwtApplication {
 
     public static void main(String[] args) {
